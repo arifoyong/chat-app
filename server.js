d8d66d058a354199de2ef69db5583eb9c61ab554
arifoyong <arifoyong@gmail.com>
2017-10-15 07:13:18 +0800

    Delete unnecessary comment

--- server.js
+++ server.js
@@ -1,31 +1,9 @@
-// load environment variables
-// require('dotenv').config();
-
 // configure dependencies
 const express = 		require('express'),
 	app = 				express(),
-	port = 				process.env.PORT || 8080,
+	port = 				process.env.PORT || 8081,
 	expressLayouts = 	require('express-ejs-layouts');
 	
-	// mongoose = 			require('mongoose'),
-	// bodyParser = 		require('body-parser'),
-	// session = 			require('express-session'),
-	// cookieParser = 		require('cookie-parser'),
-	// flash = 			require('connect-flash'),
-	// expressValidator = require('express-validator') ;
-
-// configure application =======================
-// set session & cookie parser
-// app.use(cookieParser());
-// app.use(session({
-// 	secret: process.env.SECRET,
-// 	cookie: { maxAge: 60000 },
-// 	resave: false,			//forces the session to be saved back to store	
-// 	saveUninitialized: false	//dont save unmodified
-// }))
-// app.use(flash());
-
-
 // tell express where to look for static assets
 app.use(express.static(__dirname + '/public'));
 
@@ -33,11 +11,8 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
-
-
 // set the routes
 app.use(require('./app/route'));
-
 
 
 // start our server
